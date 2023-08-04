d28af1cec38fbfc72d3116ed5569a72f8f9c2a0e
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-08-04 13:43:26 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeFixação_MediaDeAluno
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public float N1;
+        public float N2;
+        public float N3;
+
+        public float NotaFinal() { 
+            return N1 + N2 + N3;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ExercicioDeFixação_MediaDeAluno;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--- CALCULADORA DE MÉDIA ---");
+
+        Aluno a;
+        a = new Aluno();                
+
+        Console.WriteLine();
+        Console.Write("Nome do aluno: ");
+        a.Nome = Console.ReadLine();
+        Console.WriteLine("Digite as três notas do aluno: (1° - max de 30.00 pontos / 2° - max 35.00 pontos / 3° - max 35.00 pontos)");
+        Console.Write("Primeira nota: ");
+        a.N1 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Segunda nota: ");
+        a.N2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+        Console.Write("Terceira nota: ");
+        a.N3 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        float notafinal = a.NotaFinal();
+        float ponto = 60 - a.NotaFinal();
+
+        Console.WriteLine();
+        Console.Write("Nota final: " + notafinal.ToString("f2"), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        if (notafinal > 60)
+        {
+            Console.WriteLine("Parabéns! Você está aprovado.");
+        }
+        else
+        {
+            Console.WriteLine("Que pena! Você foi reprovado.");
+            Console.WriteLine("Faltaram " + ponto.ToString("f2", CultureInfo.InvariantCulture) +(" pontos."));
+        }
+    }
+}
